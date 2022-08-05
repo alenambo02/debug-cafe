2b02b6718a5648b8e38c5e90128db851b1e48ab1
Ale i <allleizq@gmail.com>
2022-08-05 11:42:53 -0700

    added signupform function to login.js

--- public/js/login.js
+++ public/js/login.js
@@ -19,12 +19,33 @@ const loginForm = async (event) => {
   }
 };
   
- 
+const signupForm = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('api user login route here', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+  
+    if (response.ok) {
+        document.location.replace('/menu');
+    } else {
+        alert(response.statusText);
+    }
+  }
+};
 
 document
     .querySelector('.login-form')
     .addEventListener('submit', loginForm);
     
-
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupForm);
 
   
