07de8492e2ca57d451fd80371223f6010e327d8e
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-08-05 12:51:19 -0700

    categories

--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -2,12 +2,28 @@ const { Category } = require('../models')
 
 const categoryData = [
     {
-        "category_name" : Drink,
+        "category_name" : "Drink",       //1
+    },
+    {
+        "category_name" : "Coffee",      //2
+    },
+    {
+        "category_name" : "Tea",         //3
+    },
+    {
+        "category_name" : "Hot",         //4
+    },
+    {
+        "category_name" : "Cold",        //5
+    },
+    {
+        "category_name" : "Caffeinated", //6
+    },
+    {
+        "category_name" : "Food",        //7
     },
 ]
 
+const seedCategories = () => Category.bulkCreate(categoryData);
 
-
-const seedItems = () => Item.bulkCreate(itemData);
-
-module.exports = seedItems
+module.exports = seedCategories
--- seeds/index.js
+++ seeds/index.js
@@ -11,14 +11,23 @@ const seedDatabase = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
 
-//   await seedUsers();
-//   console.log('\n----- USERS SEEDED -----\n');
+  await seedUsers();
+  console.log('\n----- USERS SEEDED -----\n');
 
-//   await seedPosts();
-//   console.log('\n----- POSTS SEEDED -----\n');
+  await seedItems();
+  console.log('\n----- Items SEEDED -----\n');
 
-//   await seedComments();
-//   console.log('\n----- Comments SEEDED -----\n');
+  await seedCategories();
+  console.log('\n----- Categories SEEDED -----\n');
+
+  await seedOrders();
+  console.log('\n----- Orders SEEDED -----\n');
+
+  await seedCategoryItems();
+  console.log('\n----- Item Category SEEDED -----\n');
+
+  await seedOrderItems();
+  console.log('\n----- Order Items SEEDED -----\n');
 
   process.exit(0);
 };
