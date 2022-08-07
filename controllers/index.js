524efbcf9695048e307a998040011e6dabcc78d2
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-08-07 02:40:03 -0700

    message

--- controllers/index.js
+++ controllers/index.js
@@ -11,12 +11,8 @@ const confirmOrderRoutes = require('./confirm-routes');
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/menu', menuRoutes);
-<<<<<<< HEAD
-=======
 router.use('/orders', orderRoutes);
->>>>>>> a38423135caf72049504ba3a0b1932ddbfde7ea4
 router.use('/account', accountRoutes);
-router.use('/orders', orderRoutes);
 router.use('/confirm', confirmOrderRoutes);
 
 module.exports = router;
