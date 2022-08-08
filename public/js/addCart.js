730cf73caf5c2c645f080a7ac2be29090cc42a75
C K <ctkoo@ucsc.edu>
2022-08-08 12:49:42 -0700

    createCart pending

--- public/js/addCart.js
+++ public/js/addCart.js
@@ -6,12 +6,8 @@ const addToCart = async (event) => {
   
     if (item_name && price) {
       const response = await fetch(`/api/item`, {
-        method: 'POST',
-        body: JSON.stringify(
-          { 
-            item_name,
-            price,
-         }),
+        method: 'PUT',
+        body: JSON.stringify({item_name, price,}),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -23,8 +19,35 @@ const addToCart = async (event) => {
         alert('Failed to add items to cart, please try again.');
       }
     }
-}
+};
 
 document
     .querySelector('.addtocart')
     .addEventListener('submit', addToCart);
+
+const createCart = async (event) => {
+    event.preventDefault();
+  
+    const item_name = document.querySelector('#item_title')
+    const price = document.querySelector('#item_price')
+    
+    if (item_name && price) {
+      const response = await fetch(`/api/cart`, {
+        method: 'POST',
+        body: JSON.stringify({item_name, price,}),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+};
+
+document
+    .querySelector('.addtocart')
+    .addEventListener('submit', createCart);
