72411306ff8cb95c81afe7e3f40dfb73378fe058
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-08-09 00:10:00 -0700

    fixes to addCart.js

--- public/js/addCart.js
+++ public/js/addCart.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
     const item_name = document.querySelector('#item_title')
     const price = document.querySelector('#item_price')
   
@@ -50,46 +49,3 @@ const createCart = async (event) => {
 document
     .querySelector('.addtocart')
     .addEventListener('submit', createCart);
-=======
-
-document.addEventListener("DOMContentLoaded", function(event) { 
-	var buttonEl = document.getElementsByClassName('addtocart')
-	for (var i = 0 ; i < buttonEl.length; i++) {
-		buttonEl[i].addEventListener('click' , addToCart ) ; 
-	}
-})
-
-
-const addToCart = async(e) => {
-	var item_id = Number(e.target.getAttribute('data-id'))
-	var response = await fetch(`/api/carts/${order_id}`)
-	var data = await response.json()
-	const idArr = itemArray(data.items)
-	idArr.push(item_id)
-	const update = await fetch(`/api/carts/${order_id}`, {
-		method: 'PUT',
-		body: JSON.stringify({ user_id: user_id, completed:false, itemIds: idArr }),
-		headers: { 'Content-Type': 'application/json' },
-	});
-	// var complete = await update.json()
-	// location.reload()
-	window.location.href = '/menu'
-	alert('Item Added To Cart')
-}
-
-function itemArray(arr){
-	var itemIdArr = []
-	for(let i = 0; i < arr.length; i++){
-		itemIdArr.push(arr[i].cartItem.item_id)
-	}
-	// console.log(itemIdArr)
-	return itemIdArr
-}
-
-// api/carts/order_id
-// {
-// 	"user_id": userid,
-// 	"completed": false,
-// 	"itemIds": []
-// }
->>>>>>> c8765d582e7e9fda7682af8279aa9571d2b59c9f
